badb4fa3a292cf57b544bb9bcf7f895bad0bbc28
nicolasdelafuente <48973434+nicolasdelafuente@users.noreply.github.com>
2020-07-20 16:18:47 -0300

    Iniciando Persona

---
+++ src/components/layout/BarraEstado.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Barra = () => {
+  return <div className="barraEstado">&nbsp;</div>;
+};
+
+export default Barra;
--- src/components/proyectos/Persona.js
+++ src/components/proyectos/Persona.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Barra from "../layout/Barra";
+import BarraEstado from "../layout/BarraEstado";
 
 const Persona = () => {
-  return <h1>Desde Persona</h1>;
+  return (
+    <Fragment>
+      <Barra />
+      <BarraEstado />
+      <div className="back-persona">
+        <div className="grid-per">
+          <div>Ricardo Enrique Bochini</div>
+          <div>Teléfono movil</div>
+          <div>Teléfono fijo</div>
+          <div>E-mail</div>
+          <div>15-4444-4444</div>
+          <div>4444-4444</div>
+          <div>ricardobochini@independiente.com.ar</div>
+          <div>icono mas</div>
+          <div>icono edit</div>
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Persona;
