3f98e1c97bda3c2e9d53fa5817ea531deccdcd68
nicolasdelafuente <48973434+nicolasdelafuente@users.noreply.github.com>
2020-07-21 09:59:51 -0300

    CSS Persona

--- src/components/proyectos/Persona.js
+++ src/components/proyectos/Persona.js
@@ -9,15 +9,84 @@ const Persona = () => {
       <BarraEstado />
       <div className="back-persona">
         <div className="grid-per">
-          <div>Ricardo Enrique Bochini</div>
-          <div>Teléfono movil</div>
-          <div>Teléfono fijo</div>
-          <div>E-mail</div>
-          <div>15-4444-4444</div>
-          <div>4444-4444</div>
-          <div>ricardobochini@independiente.com.ar</div>
-          <div>icono mas</div>
-          <div>icono edit</div>
+          <div>
+            <h1>Ricardo Enrique Bochini</h1>
+          </div>
+          <div className="nested-grid">
+            <div>
+              <h5>Teléfono movil</h5>
+            </div>
+            <div>
+              <h3>15-4444-4444</h3>
+            </div>
+          </div>
+          <div className="nested-grid">
+            <div>
+              <h5>Teléfono fijo</h5>
+            </div>
+            <div>
+              <h3>4444-4444</h3>
+            </div>
+          </div>
+          <div className="nested-grid">
+            <div>
+              <h5>E-mail</h5>
+            </div>
+            <div>
+              <h3>ricardobochini@independiente.com.ar</h3>
+            </div>
+          </div>
+          <div className="icon-grid">
+            <div>
+              <div>
+                <i class="fas fa-plus"></i>
+              </div>
+              <div>
+                <i class="far fa-edit"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="back-persona">
+        <div className="grid-per">
+          <div>
+            <h1>Seguimiento: 00010</h1>
+          </div>
+          <div>
+            <h3>15-4444-4444</h3>
+          </div>
+          <div>
+            <h3>Entrevista 3</h3>
+          </div>
+          <div>
+            <h3>Próxima Reunión</h3>
+          </div>
+          <div className="icon-grid">
+            <div>
+              <div>
+                <i class="fas fa-plus"></i>
+              </div>
+              <div>
+                <i class="far fa-edit"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="back-persona">
+          <div className="grid-per">
+            <h2>Motivo</h2>
+            <div className="icon-grid">
+              <div>
+                <div>
+                  <i class="fas fa-plus"></i>
+                </div>
+                <div>
+                  <i class="far fa-edit"></i>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Fragment>
